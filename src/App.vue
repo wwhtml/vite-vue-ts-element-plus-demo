be9ff2da40f525dbcd33cf583b3671b6fc311222
wwhtml <1129942397@qq.com>
2023-11-10 01:25:10 +0800

    提交示例

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+// import { ElMessage } from 'element-plus'
+
+const alertMessage = () => {
+  ElMessage.success({
+    message: 'element'
+  })
+}
+</script>
+
+<template>
+  <div>
+    <el-button @click="alertMessage">sssss</el-button>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
